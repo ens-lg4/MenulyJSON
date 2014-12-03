910d0a1fe921522f19951bba5822b834caa2dab6
Leo Gordon <lg4@ebi.ac.uk>
2014-12-03 16:11:47 +0000

    simplified the code generator - removed the generic bit

--- menuly_codegen.js
+++ menuly_codegen.js
@@ -6,7 +6,7 @@ Blockly.JSON = new Blockly.Generator('JSON');
 
 Blockly.JSON['start'] = function(block) {
 
-    var json    = this.generalBlockToObj( block.getInputTargetBlock( 'json' ), false );
+    var json    = this.generalBlockToObj( block.getInputTargetBlock( 'json' ) );
 
     return json;
 }
@@ -41,7 +41,7 @@ Blockly.JSON['dictionary'] = function(block) {
 
     for (var i = 0; i<block.length; i++) {
         var pair_key    = block.getFieldValue( 'key_field_'+i );
-        var pair_value  = this.generalBlockToObj( block.getInputTargetBlock( 'element_'+i ), false );
+        var pair_value  = this.generalBlockToObj( block.getInputTargetBlock( 'element_'+i ) );
 
         dictionary[pair_key] = pair_value;
     }
@@ -55,7 +55,7 @@ Blockly.JSON['array'] = function(block) {
     var array = [];
 
     for (var i = 0; i<block.length; i++) {
-        var element_value  = this.generalBlockToObj( block.getInputTargetBlock( 'element_'+i ), false );
+        var element_value  = this.generalBlockToObj( block.getInputTargetBlock( 'element_'+i ) );
 
         array[i] = element_value;
     }
@@ -64,7 +64,7 @@ Blockly.JSON['array'] = function(block) {
 }
 
 
-Blockly.JSON.generalBlockToObj = function(block, returnarray) {
+Blockly.JSON.generalBlockToObj = function(block) {
 
     if(block) {
 
@@ -72,56 +72,9 @@ Blockly.JSON.generalBlockToObj = function(block, returnarray) {
         var func = this[block.type];
         if(func) {
             return func.call(this, block);
-        }
-
-        var obj = {
-            'ID'    : block.id,
-            'CLASS' : block.type,
-        };
-
-        returnarray = returnarray || (block.nextConnection ? true : false);
-
-        for (var i = 0, inputLine; inputLine = block.inputList[i]; i++) {
-
-            for (var y = 0, field; field = inputLine.fieldRow[y]; y++) {
-                if (field.name && field.EDITABLE) {
-                    // inputnode.fields[field.name] = field.getValue();     // the direct way assumes we scan through fields in a 2D order (all fields or all inputLines)
-                                                                            //
-                    obj[field.name] = block.getFieldValue( field.name );    // however I'd like to make sure we can access the same data directly, by using field's name
-                                                                            // it works!!!
-                }
-            }
-
-            if (inputLine.type != Blockly.DUMMY_INPUT) {        // dummy means there may be fields, but no slots
-                // var inputBlock = inputLine.connection.targetBlock();      // same trick here - we'd like to access an inputBlock using its name
-
-                var inputBlock = block.getInputTargetBlock( inputLine.name );
-
-
-                if (inputLine.type == Blockly.INPUT_VALUE) {
-                    obj[inputLine.name + '(value)'] = this.generalBlockToObj( inputBlock, false );
-
-                } else if (inputLine.type == Blockly.NEXT_STATEMENT) {
-                    obj[inputLine.name + '(statement)'] = this.generalBlockToObj( inputBlock, false );
-                }
-            }
-        }
-
-        if( returnarray ) {
-            var nextBlock = block.getNextBlock();
-            if (nextBlock) {
-                var tail = this.generalBlockToObj( nextBlock, true );
-                tail.unshift( obj );
-                return tail;
-            } else {
-                return [ obj ];
-            }
         } else {
-            return obj;
+            console.log("Don't know how to generate JSON code for a '"+block.type+"'");
         }
-
-    } else if( returnarray ) {
-        return [];
     } else {
         return null;
     }
@@ -138,7 +91,7 @@ Blockly.JSON.workspaceToJSON = function(workspace, block_id) {
 
     for (var i = 0, block; block = blocks[i]; i++) {
 
-        var obj = this.generalBlockToObj( block, false );
+        var obj = this.generalBlockToObj( block );
         json_text += JSON.stringify(obj, null, 4) + '\n\n';
     }
 
